889ebe4524ffc85ca6c366469739b32722397beb
Admin <Admin@wangyiliang.aspire.aspire-tech.com>
2020-03-18 10:14:39 +0800

    springboot启动日志图形是否开启

--- src/main/java/com/reven/SpringBootDemoApplication.java
+++ src/main/java/com/reven/SpringBootDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.Banner;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -20,7 +21,14 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		// 代码中配置秘钥
+//      System.setProperty("jasypt.encryptor.password", "Yghsakdgh");
+        /** 方法一：关闭springboot LOGO日志打印 */
+        SpringApplication springApplication = new SpringApplication(SpringBootDemoApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+        /** 方法二：默认开启springboot LOGO日志打印 */
+//		SpringApplication.run(CmopWebApplication.class, args);
 	}
 
 }
